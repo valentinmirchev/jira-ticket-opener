f797afaa0e525fdcbd8fe8549ac793f767c293f8
mirchval <valentin.mirchev@mercedes-benz.io>
2021-07-02 15:38:25 +0100

    feat: add multiple project support

--- background.js
+++ background.js
@@ -1,26 +1,35 @@
-openTicket = async ( { selectionText }) => {
-  let baseUrl = '';
-  let mainProject = '';
-  chrome.storage.sync.get("baseURL", (value) => {
-    console.log(value);
-    baseUrl = Object.values(value)[0];
-
-    chrome.storage.sync.get("mainProject", (value) => {
-      console.log(value);
-      mainProject = Object.values(value)[0];
-
-      if (!!mainProject && !!baseUrl) {
-        const url = `${baseUrl}${mainProject}-${selectionText}`;
-        console.log(url);
-        chrome.tabs.create({url});
-      }
+const addProjects = (projects) => {
+  projects.forEach(project => {
+    const id = project.trim();
+    chrome.contextMenus.create({
+      title: `Open for ${id}`,
+      id,
+      contexts:["selection"]
     });
+  })
+}
+
+const getCurrentTab = async () => {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+const openTicket = async ({ selectionText, menuItemId }) => {
+  chrome.storage.sync.get(['baseURL'], async (result) => {
+    const baseUrl = result.baseURL;
+
+    if (!!baseUrl) {
+      const url = `${baseUrl}${menuItemId}-${selectionText}`;
+      const currentTab = await getCurrentTab();
+      chrome.tabs.create({url, index: currentTab.index + 1});
+    }
   });
 };
 
-chrome.contextMenus.onClicked.addListener(openTicket)
-chrome.contextMenus.create({
-  title: "Open JIRA Ticket",
-  id: "default",
-  contexts:["selection"]
+chrome.storage.sync.get(['projects'], (result) => {
+  const projects = result.projects.split(',')
+  addProjects(projects);
 });
+
+chrome.contextMenus.onClicked.addListener(openTicket)
